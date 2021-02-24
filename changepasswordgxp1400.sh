c424f57099b390f518735eaf6a319ffaccda9153
root <root@otg.aster.kz>
2021-02-24 14:58:28 +0600

    Add Changer Upgrade server for GXP1400

--- changepasswordgxp1400.sh
+++ changepasswordgxp1400.sh
@@ -1,14 +1,26 @@
 #!/bin/bash
 
-hardpassword=${2}
+ip_address=${1}
+currentpassword=${2}
+hardpassword=${3}
 
-sid=$(curl -s -c /root/cookies.txt -d"password=admin" http://${1}/cgi-bin/dologin | grep sid |sed -r 's|.*"sid" : "([0-9a-z]+)".*|\1|' )
+sid=$(curl -s -c /root/cookies.txt -d"password=$currentpassword" http://$ip_address/cgi-bin/dologin | grep sid | sed -r 's|.*"sid"[[:space:]]*: "([0-9a-z]+)".*|\1|' )
 if [ ! -z $sid ]
 then
-    echo "OK Login ${1}"
-    changepassword="P196=$hardpassword&:confirmUserPwd=$hardpassword&P2=$hardpassword&:confirmAdminPwd=$hardpassword&sid=${sid}"
-    curl -b /tmp/cookies.txt -d"${changepassword}" http://${1}/cgi-bin/api.values.post
+  echo "OK Login ${1}"
+  changepassword="P196=$hardpassword&:confirmUserPwd=$hardpassword&P2=$hardpassword&:confirmAdminPwd=$hardpassword&sid=${sid}"
+  curl -b /tmp/cookies.txt -d"${changepassword}" http://${1}/cgi-bin/api.values.post
+  applaycmd="cmd=extend&arg=&sid=${sid}"
+  curl -s -X "POST" -b /tmp/cookies.txt -d"${applaycmd}" --header "Connection: keep-alive" \
+    --header "Cache-Control: max-age=0" \
+    --header "Origin: http://$ip_address" \
+    --header "Referer: http://$ip_address/" \
+    --header "ccept-Encoding: gzip, deflate" \
+    --header "Accept-Language: ru-RU,ru;q=0.8,en-US;q=0.6,en;q=0.4" \
+    --header "User-Agent: Chrome/58.0.3029.110 Safari/537.36" \
+    "http://$ip_address/cgi-bin/api-phone_operation"
+
 else
-    echo "Wrong Password ${1}"
+    echo "Wrong Password $ip_address"
 fi
 #curl -b /tmp/cookies.txt -d"request=REBOOT&sid=${sid}" http://${2}/cgi-bin/api-sys_operation
---
+++ changeupdateserver1400.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+ip_address=${1}
+hardpassword=${2}
+upgradehttpserver=${3}
+
+sid=$(curl -s -c /tmp/cookies.txt -d"password=$hardpassword" -H "Referer: http://$ip_address/" http://$ip_address/cgi-bin/dologin | grep sid | sed -r 's|.*"sid"[[:space:]]*: "([0-9a-z]+)".*|\1|' )
+if [ ! -z $sid ]
+then
+  echo "OK Login $ip_address"
+  changeupgradesettings="P238=0&P212=1&P192=$upgradehttpserver&P194=3&&sid=${sid}"
+  curl -s -X "POST" -b /tmp/cookies.txt -d"${changeupgradesettings}" --header "Connection: keep-alive" \
+    --header "Cache-Control: max-age=0" \
+    --header "Origin: http://$ip_address" \
+    --header "Referer: http://$ip_address/" \
+    --header "ccept-Encoding: gzip, deflate" \
+    --header "Accept-Language: ru-RU,ru;q=0.8,en-US;q=0.6,en;q=0.4" \
+    --header "User-Agent: Chrome/58.0.3029.110 Safari/537.36" \
+    "http://$ip_address/cgi-bin/api.values.post"
+  applaycmd="cmd=extend&arg=&sid=${sid}"
+  curl -s -X "POST" -b /tmp/cookies.txt -d"${applaycmd}" --header "Connection: keep-alive" \
+    --header "Cache-Control: max-age=0" \
+    --header "Origin: http://$ip_address" \
+    --header "Referer: http://$ip_address/" \
+    --header "ccept-Encoding: gzip, deflate" \
+    --header "Accept-Language: ru-RU,ru;q=0.8,en-US;q=0.6,en;q=0.4" \
+    --header "User-Agent: Chrome/58.0.3029.110 Safari/537.36" \
+    "http://$ip_address/cgi-bin/api-phone_operation"
+  curl -s -X "POST" -b /tmp/cookies.txt -d"request=REBOOT&sid=${sid}" \
+    --header "Cache-Control: max-age=0" \
+    --header "Origin: http://$ip_address" \
+    --header "Referer: http://$ip_address/" \
+    --header "ccept-Encoding: gzip, deflate" \
+    --header "Accept-Language: ru-RU,ru;q=0.8,en-US;q=0.6,en;q=0.4" \
+    --header "User-Agent: Chrome/58.0.3029.110 Safari/537.36" \
+    http://$ip_address/cgi-bin/api-sys_operation
+else
+    echo "Wrong Password $ip_address"
+fi
